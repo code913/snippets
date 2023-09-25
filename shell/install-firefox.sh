7e0f08eeacbb569b98a05fbf9f5c89fed3dc3d2d
code913 <54856929+code913@users.noreply.github.com>
2023-09-25 21:36:25 +0530

    feat: select edition
    yeah i used chatgpt dont judge me

--- shell/install-firefox.sh
+++ shell/install-firefox.sh
@@ -2,11 +2,30 @@
 
 # Installs firefox dev edition for linux 64-bit with language en-US
 # i have extremely little knowledge of bash so stuff may break
-curl "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US" -L --output firefox-archive.tar.bz2
+declare -A editions
+editions["Firefox"]="firefox-latest-ssl"
+editions["Firefox Beta"]="firefox-beta-latest-ssl"
+editions["Firefox Developer Edition"]="firefox-devedition-latest-ssl"
+editions["Firefox Nightly"]="firefox-nightly-latest-ssl"
+editions["Firefox Extended Support Release"]="firefox-esr-latest-ssl"
+
+keys=("${!editions[@]}")
+editionId=${editions["Firefox"]}
+
+PS3="Which firefox edition do you want to use? "
+select edition in "${keys[@]}"; do
+    if [ -n "$edition" ]; then
+        editionId=${editions["$edition"]}
+    else
+        echo "Invalid choice, selecting Firefox"
+    fi
+    break
+done
+
+curl "https://download.mozilla.org/?product=${editionId}&os=linux64&lang=en-US" -L --output firefox-archive.tar.bz2
 bunzip2 firefox-archive.tar.bz2
 tar xvf firefox-archive.tar
 
-clear
 echo "Edit ~/.bashrc and add the following at the end:"
 echo "alias firefox=\"<the dir you just installed firefox>/firefox\""
 echo "Then type firefox in the terminal to launch firefox"
