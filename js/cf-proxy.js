628362d8713e5396730930b179cfa3796ae75513
code913 <54856929+code913@users.noreply.github.com>
2023-10-17 21:50:36 +0530

    refactor: unnecessary response constructor

--- js/cf-proxy.js
+++ js/cf-proxy.js
@@ -3,10 +3,10 @@ export default {
   async fetch(request, env, ctx) {
     const url = new URL(new URL(request.url).searchParams.get("url"));
     console.log(url);
-    return new Response(await fetch(url.href, {
+    return await fetch(url.href, {
       body: await request.arrayBuffer(),
       method: request.method,
       headers: request.headers
-    }));
+    });
   },
 };
