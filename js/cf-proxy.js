954c2a7be9ad3c430135ce86b9697c4297758a25
code913 <54856929+code913@users.noreply.github.com>
2023-10-20 16:35:49 +0530

    fix: body on GET/HEAD request

--- js/cf-proxy.js
+++ js/cf-proxy.js
@@ -3,10 +3,11 @@ export default {
   async fetch(request, env, ctx) {
     const url = new URL(new URL(request.url).searchParams.get("url"));
     console.log(url);
-    return await fetch(url.href, {
-      body: await request.arrayBuffer(),
+    const payload = {
       method: request.method,
       headers: request.headers
-    });
+    };
+    if (!["GET", "HEAD"].includes(payload.method)) payload.body = await request.arrayBuffer();
+    return await fetch(url, payload);
   },
 };
