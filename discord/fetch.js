fc203839a5e83926b0c4a2dcbbeb6c64f519b622
code913 <54856929+code913@users.noreply.github.com>
2023-10-26 19:41:34 +0530

    docs: YOU WILL USE JSON AND YOU WILL BE HAPPY

--- discord/fetch.js
+++ discord/fetch.js
@@ -1,5 +1,6 @@
 /**
  * Boilerplate to make a request to discord api
+ * Note: this does not handle file uploads; check interaction.ts for an example on how to handle files
  */
 dapi = (path, token, method = "GET", body, parse = true) => fetch(`https://discord.com/api/v10/${path}`, {
     headers: {
