d48b8da33423dfb08dd216fef4d11a7f9a1fa613
code913 <54856929+code913@users.noreply.github.com>
2023-12-21 16:24:27 +0530

    feat: url rewrites for cf-proxy responses

--- js/cf-proxy.js
+++ js/cf-proxy.js
@@ -1,13 +1,55 @@
-// Extremely simple redirect through cloudflare workers
+/**
+  * Redirect requests through cloudflare workers and try to rewrite response urls to also go through it
+  * @see https://proxy.code913.workers.dev/https://code913.devpage.me
+  */
+function rewriteUrls(targetUrl, text) {
+  const workerUrl = "https://proxy.code913.workers.dev/";
+  const modifiedUrl = workerUrl + encodeURI(new URL(targetUrl).origin);
+
+  // Inject <base> tag into the <head>
+  return text
+    .replace(/(<head>)/i, `$1<base href="${modifiedUrl}/">`)
+    .replace(/(href|src)=(['"]?)([^"'\s>]+)(['" >])/gi, (match, attribute, quote1, url, quote2) => {
+    if (url.startsWith("//")) {
+      // Protocol-relative URL: Join with worker's origin
+      url = modifiedUrl + url.slice(2);
+    } else if (!url.startsWith("http")) {
+      // Relative path: Join with worker's pathname
+      url = modifiedUrl + url;
+    } else if (!url.startsWith(modifiedUrl)) {
+      // Absolute URL not pointing to the worker: Rewrite
+      url = workerUrl + encodeURI(url);
+    }
+    return `${attribute}=${quote1}${url}${quote2}`;
+  });
+}
+
 export default {
+  /**
+   * @param {Request} request
+   */
   async fetch(request, env, ctx) {
-    const url = new URL(new URL(request.url).searchParams.get("url"));
-    console.log(url);
+    const url = new URL(request.url);
+    const targetUrl = new URL(url.pathname !== "/" ? url.pathname.slice(1) : url.searchParams.get("url")).href; // Handle both pathname and ?url cases
+
+    console.log(targetUrl);
+
     const payload = {
       method: request.method,
-      headers: request.headers
+      headers: request.headers,
     };
-    if (!["GET", "HEAD"].includes(payload.method)) payload.body = await request.arrayBuffer();
-    return await fetch(url, payload);
+    if (!["GET", "HEAD"].includes(payload.method)) {
+      payload.body = await request.arrayBuffer();
+    }
+
+    const response = await fetch(targetUrl, payload);
+
+    if (response.headers.get("Content-Type").startsWith("text/")) {
+      const text = await response.text();
+      const modifiedText = rewriteUrls(targetUrl, text);
+      return new Response(modifiedText, response);
+    } else {
+      return response;
+    }
   },
 };
