04023c6d85098a19a96862d5f597825d495c1163
code913 <54856929+code913@users.noreply.github.com>
2023-12-21 18:13:22 +0530

    feat: ignore[] param to not rewrite certain urls + refactor a bit

--- js/cf-proxy.js
+++ js/cf-proxy.js
@@ -1,24 +1,29 @@
 /**
   * Redirect requests through cloudflare workers and try to rewrite response urls to also go through it
-  * @see https://proxy.code913.workers.dev/https://code913.devpage.me
+  * No guarantee as to whether it would work or get blocked
+  * @see https://proxy.code913.workers.dev/https://code913.devpage.me/?&ignore[]=github.com&ignore[]=svelte.dev&ignore[]=codepen.io&ignore[]=discord.gg
   */
-function rewriteUrls(targetOrigin, text) {
-  const workerUrl = "https://proxy.code913.workers.dev/";
-  const modifiedUrl = workerUrl + encodeURI(targetOrigin.origin);
+const workerUrl = "https://proxy.code913.workers.dev/";
+
+function rewriteUrls(targetOrigin, text, ignore) {
+  const modifiedUrl = workerUrl + encodeURI(targetOrigin);
 
   // Inject <base> tag into the <head>
   return text
-    .replace(/(<head>)/i, `$1<base href="${modifiedUrl}/">`)
+    .replace(/(<head[^>]*?>)/i, `$1<base href="${modifiedUrl}/">`)
     .replace(/(href|src)=(['"]?)([^"'\s>]+)(['" >])/gi, (match, attribute, quote1, url, quote2) => {
-      if (url.startsWith("//")) {
-        // Protocol-relative URL: Join with worker's origin
-        url = modifiedUrl + url.slice(2);
-      } else if (!url.startsWith("http")) {
-        // Relative path: Join with worker's pathname
-        url = modifiedUrl + url;
-      } else if (!url.startsWith(modifiedUrl)) {
-        // Absolute URL not pointing to the worker: Rewrite
-        url = workerUrl + encodeURI(url);
+      if (!ignore.some(i => url.includes(i))) {
+        if (url.startsWith("//")) {
+          // Protocol-relative URL: Join with worker's origin
+          console.trace({ url });
+          url = workerUrl + "https://" + url.slice(2);
+        } else if (!url.startsWith("http")) {
+          // Relative path: Join with worker's pathname
+          url = modifiedUrl + url;
+        } else if (!url.startsWith(modifiedUrl)) {
+          // Absolute URL not pointing to the worker: Rewrite
+          url = workerUrl + encodeURI(url);
+        }
       }
       return `${attribute}=${quote1}${url}${quote2}`;
     });
@@ -28,8 +33,9 @@ export default {
   /**
    * @param {Request} request
    */
-  async fetch(request, env, ctx) {
+  async fetch(request) {
     const url = new URL(request.url);
+    const ignore = url.searchParams.getAll("ignore[]");
     const targetOrigin = new URL(url.pathname !== "/" ? url.pathname.slice(1) : url.searchParams.get("url")).href; // Handle both pathname and ?url cases
 
     const payload = {
@@ -46,10 +52,10 @@ export default {
 
     if (response.headers.get("Content-Type").startsWith("text/")) {
       const text = await response.text();
-      const modifiedText = rewriteUrls(targetOrigin, text);
+      const modifiedText = rewriteUrls(targetOrigin, text, ignore);
       return new Response(modifiedText, response);
-    } else {
-      return response;
     }
+    
+    return response;
   },
 };
